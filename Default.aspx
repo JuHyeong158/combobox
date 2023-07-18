4ec87ad624f4adae49ba5cbd60f70b9b4fe6b818
JuHyeong158 <110215982+JuHyeong158@users.noreply.github.com>
2023-07-18 17:42:01 +0900

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Temp.WebTemplate.Edu.jhbae.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="ddlFruits" runat="server" DataTextField="FruitName" DataValueField="FruitID"></asp:DropDownList>
+            <br />
+            <asp:Button ID="btnSubmit" runat="server" Text="선택 확인" OnClick="btnSubmit_Click" />
+            <br />
+            <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.UI.WebControls;
+
+namespace Temp.WebTemplate.Edu.jhbae
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindDropDownList();
+            }
+        }
+        protected void BindDropDownList()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["JINI_EDU"].ConnectionString; ; 
+
+            string procedureName = "GetFruits";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(procedureName, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataSet dataSet = new DataSet();
+
+                    adapter.Fill(dataSet);
+
+                    ddlFruits.DataSource = dataSet;
+                    ddlFruits.DataBind();
+
+                    ddlFruits.Items.Insert(0, new ListItem("선택하세요", "0"));
+                }
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string selectedText = ddlFruits.SelectedItem.Text;
+            string selectedValue = ddlFruits.SelectedItem.Value;
+
+            lblResult.Text = $"선택된 과일: {selectedText}, 값: {selectedValue}";
+        }
+
+    }
+}
